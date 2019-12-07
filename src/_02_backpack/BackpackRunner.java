ccd146780501b68116c37328aa5abcb5756c094a
League Student <league@iMac-24.attlocal.net>
2019-12-07 11:26:16 -0800

    SOMEwhere at start
    :<[]

--- src/_02_backpack/BackpackRunner.java
+++ src/_02_backpack/BackpackRunner.java
@@ -2,6 +2,14 @@ package _02_backpack;
 
 public class BackpackRunner {
 	public static void main(String[] args) {
-		new Backpack().packAndCheck();
+		Backpack bapa = new Backpack();
+		Pencil penc = new Pencil();
+		Ruler rule = new Ruler();
+		Textbook txtbo = new Textbook();
+		bapa.putInBackpack(penc);
+		bapa.putInBackpack(rule);
+		bapa.putInBackpack(txtbo);
+		bapa.packAndCheck();
+		/* new Backpack().packAndCheck(); */
 	}
 }
